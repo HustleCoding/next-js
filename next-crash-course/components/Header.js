6412a046db517f3a5badf8f8609f03c2a420cb5d
FlorinDobinciuc <florin.dobinciuc@thinslices.com>
2022-01-07 12:10:26 +0200

    [new files added]

--- next-crash-course/components/Header.js
+++ next-crash-course/components/Header.js
@@ -3,9 +3,12 @@ import headerStyles from "../styles/Header.module.css";
 const Header = () => {
   return (
     <div>
-      <h1>
-        <span>Webdev News</span>
+      <h1 className={headerStyles.title}>
+        <span>WebDev</span> News
       </h1>
+      <p className={headerStyles.description}>
+        Keep up to date with the latest web dev news
+      </p>
     </div>
   );
 };
--- next-crash-course/pages/index.js
+++ next-crash-course/pages/index.js
@@ -1,6 +1,6 @@
 import Head from "next/head";
 
-export default function Home() {
+export default function Home({ articles }) {
   return (
     <div>
       <Head>
@@ -8,7 +8,22 @@ export default function Home() {
         <meta name="bla bla" content="web dev bla bla" />
       </Head>
 
-      <h1>Welcome to Next!</h1>
+      {articles.map((article) => (
+        <h3 key={article}>{article.title}</h3>
+      ))}
     </div>
   );
 }
+
+export const getStaticProps = async () => {
+  const res = await fetch(
+    `https://jsonplaceholder.typicode.com/posts?_limit=6`
+  );
+  const articles = await res.json();
+
+  return {
+    props: {
+      articles,
+    },
+  };
+};
