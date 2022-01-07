d9b3f24e7957323489ce0a80b613d39a62d0eeaa
FlorinDobinciuc <florin.dobinciuc@thinslices.com>
2022-01-07 12:16:55 +0200

    [ArticleList component added]

--- next-crash-course/pages/index.js
+++ next-crash-course/pages/index.js
@@ -1,4 +1,5 @@
 import Head from "next/head";
+import ArticleList from "../components/ArticleList";
 
 export default function Home({ articles }) {
   return (
@@ -7,10 +8,7 @@ export default function Home({ articles }) {
         <title>WebDev News</title>
         <meta name="bla bla" content="web dev bla bla" />
       </Head>
-
-      {articles.map((article) => (
-        <h3 key={article}>{article.title}</h3>
-      ))}
+      <ArticleList articles={articles} />
     </div>
   );
 }
