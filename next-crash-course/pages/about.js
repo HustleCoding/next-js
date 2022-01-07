dce4e7c4c4a8be33cf2e068854939b91d0c19d48
FlorinDobinciuc <florin.dobinciuc@thinslices.com>
2022-01-07 10:26:00 +0200

    [about page added]

---
+++ next-crash-course/pages/about.js
@@ -0,0 +1,15 @@
+import Head from "next/head";
+
+const about = () => {
+  return (
+    <div>
+      <h1>About page</h1>
+
+      <Head>
+        <title>About page here</title>
+      </Head>
+    </div>
+  );
+};
+
+export default about;
