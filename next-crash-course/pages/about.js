aaae1f296e3d61e96546702c61da344f5f5b6c02
FlorinDobinciuc <florin.dobinciuc@thinslices.com>
2022-01-10 10:48:36 +0200

    [adding images and metadata]

--- next-crash-course/pages/about.js
+++ next-crash-course/pages/about.js
@@ -1,14 +1,22 @@
+import Head from "next/head";
+
 const about = () => {
   return (
-    <div>
-      <h1>About</h1>
-      <p>
-        Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod
-        tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim
-        veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea
-        commodo consequat.
-      </p>
-    </div>
+    <>
+      <Head>
+        <title>Ninja List | About</title>
+        <meta name="keywords" content="ninjas" />
+      </Head>
+      <div>
+        <h1>About</h1>
+        <p>
+          Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do
+          eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad
+          minim veniam, quis nostrud exercitation ullamco laboris nisi ut
+          aliquip ex ea commodo consequat.
+        </p>
+      </div>
+    </>
   );
 };
 
--- next-crash-course/pages/index.js
+++ next-crash-course/pages/index.js
@@ -1,17 +1,22 @@
-import Footer from "../components/Footer";
+import Head from "next/head";
 import Link from "next/link";
-import Navbar from "../components/Navbar";
 import styles from "../styles/Home.module.css";
 
 export default function Home() {
   return (
-    <div>
-      <h1 className={styles.title}>Homepage</h1>
-      <p className={styles.text}>Lorem ipsum dolor sit amet</p>
-      <p className={styles.text}>Lorem ipsum dolor sit amet</p>
-      <Link href="/ninjas">
-        <a className={styles.btn}>See Ninja Listing</a>
-      </Link>
-    </div>
+    <>
+      <Head>
+        <title>Ninja List | Home</title>
+        <meta name="keywords" content="ninjas" />
+      </Head>
+      <div>
+        <h1 className={styles.title}>Homepage</h1>
+        <p className={styles.text}>Lorem ipsum dolor sit amet</p>
+        <p className={styles.text}>Lorem ipsum dolor sit amet</p>
+        <Link href="/ninjas">
+          <a className={styles.btn}>See Ninja Listing</a>
+        </Link>
+      </div>
+    </>
   );
 }
