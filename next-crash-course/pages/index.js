c1ddf20f2177144de9290257d912d86760247a94
FlorinDobinciuc <florin.dobinciuc@thinslices.com>
2022-01-10 09:53:21 +0200

    [Layout component added]

--- next-crash-course/pages/index.js
+++ next-crash-course/pages/index.js
@@ -5,14 +5,12 @@ import Navbar from "../components/Navbar";
 export default function Home() {
   return (
     <div>
-      <Navbar />
       <h1>Homepage</h1>
       <p>Lorem ipsum dolor sit amet</p>
       <p>Lorem ipsum dolor sit amet</p>
       <Link href="/ninjas">
         <a>See Ninja Listing</a>
       </Link>
-      <Footer />
     </div>
   );
 }
