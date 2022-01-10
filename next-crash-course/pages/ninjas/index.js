94022498b14b74731aa979772079475bb874bf39
FlorinDobinciuc <florin.dobinciuc@thinslices.com>
2022-01-10 11:07:25 +0200

    [fetching data - getStaticProps()]

--- next-crash-course/pages/ninjas/index.js
+++ next-crash-course/pages/ninjas/index.js
@@ -1,5 +1,27 @@
-const Ninjas = () => {
-  return <h1>All ninjas</h1>;
+import styles from "../../styles/Ninjas.module.css";
+
+export const getStaticProps = async () => {
+  const res = await fetch("https://jsonplaceholder.typicode.com/users");
+  const data = await res.json();
+
+  return {
+    props: { ninjas: data },
+  };
+};
+
+const Ninjas = ({ ninjas }) => {
+  return (
+    <div>
+      <h1>All Ninjas</h1>
+      {ninjas.map((ninja) => (
+        <div key={ninja.id}>
+          <a className={styles.single}>
+            <h3>{ninja.name}</h3>
+          </a>
+        </div>
+      ))}
+    </div>
+  );
 };
 
 export default Ninjas;
